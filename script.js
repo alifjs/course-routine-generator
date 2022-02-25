1770e52f77cea457c683d0567797a38dca5d1236
Alif <syedalifulalam19@gmail.com>
2022-02-26 05:23:45 +0600

    Day 2

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+//Showing the dustbin when the mouse comes over an individual course
+let individual_course=document.querySelector('.individual_course')
 
+individual_course.addEventListener('mouseover',()=>{
+    let dustbin=document.querySelector('img')
+    setTimeout(()=>{
+        dustbin.style.visibility="visible"
+    },100)
+})
+individual_course.addEventListener('mouseout',()=>{
+    let dustbin=document.querySelector('img')
+    setTimeout(()=>{
+        dustbin.style.visibility="hidden"
+    },100)
+})
+//---------------------------------------------------------------------//
+class Course{
+    constructor(name,section,days,time){
+        this.name=name
+        this.section=section
+        this.days=days
+        this.time=time
+    }
+}
+let add_button=document.querySelector('.add_button h1')
+
+add_button.addEventListener('click',()=>{
+    let course_name=document.querySelector('.course_input div:nth-child(1) input').value
+    let section=document.querySelector('.course_input div:nth-child(2) select').value
+    let days=document.querySelector('.course_input div:nth-child(3) select').value
+    let time=document.querySelector('.course_input div:nth-child(4) select').value
+
+    console.log(course_name,section,days,time)
+})
 
 
